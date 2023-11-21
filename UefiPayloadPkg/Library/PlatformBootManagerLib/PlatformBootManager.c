0b190e5fae79de7060ee39efc44821de02918ae0
nik <151679854+nikthegay@users.noreply.github.com>
2023-11-21 21:55:24 +0100

    Update PlatformBootManager.c

--- UefiPayloadPkg/Library/PlatformBootManagerLib/PlatformBootManager.c
+++ UefiPayloadPkg/Library/PlatformBootManagerLib/PlatformBootManager.c
@@ -328,7 +328,7 @@ PlatformBootManagerAfterConsole (
     BootLogoUpdateProgress (
       White,
       Black,
-      L"Press ESC for Boot Options/Settings",
+      L"booting up nik's laptop.. meow! :3",
       White,
       0,
       0
@@ -337,7 +337,7 @@ PlatformBootManagerAfterConsole (
     BootLogoUpdateProgress (
       White,
       Black,
-      L"Press F2 or Down for Boot Options/Settings",
+      L"booting up nik's laptop.. meow! :3",
       White,
       0,
       0
